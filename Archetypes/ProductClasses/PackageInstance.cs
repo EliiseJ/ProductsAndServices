a7574d737b821d0b6d1038ae2dec456ac82a81b4
EliiseJ <juhansoo.eliise@gmail.com>
2017-03-26 17:24:37 +0300

    PackageInstance klass ja testid

---
+++ Archetypes/ProductClasses/PackageInstance.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Open.Archetypes.ProductClasses
+{
+    public class PackageInstance : ProductInstance
+    {
+        public ProductInstance[] getContents() => ProductInstances.Instance.ToArray();
+    }
+}
---
+++ Tests/Archetypes/ProductClasses/PackageInstanceTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Archetypes.ProductClasses;
+
+namespace Open.Tests.Archetypes.ProductClasses
+{
+    [TestClass]
+    public class PackageInstanceTests
+    {
+        private PackageInstance instance;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            instance = new PackageInstance();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            instance = null;
+        }
+
+        [TestMethod]
+        public void getContentsTest()
+        {
+            Assert.IsNotNull(instance.getContents());
+        }
+    }
+}
