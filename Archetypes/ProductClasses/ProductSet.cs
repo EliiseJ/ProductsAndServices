7c3336ab7764a635d75f3b34e92dd19d9764b782
EliiseJ <juhansoo.eliise@gmail.com>
2017-03-26 17:37:42 +0300

    ProductSet klass ja testid

---
+++ Archetypes/ProductClasses/ProductSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Open.Archetypes.BaseClasses;
+
+namespace Open.Archetypes.ProductClasses
+{
+    public class ProductSet : Archetype
+    {
+        public string name { get; set; }
+
+        public List<ProductIdentifier> productIdentifiers { get; } = new List<ProductIdentifier>();
+
+        public void addProductReference(ProductIdentifier reference)
+        {
+            productIdentifiers.Add(reference);
+        }
+
+        public int count()
+        {
+            return productIdentifiers.Count;
+        }
+    }
+}
---
+++ Tests/Archetypes/ProductClasses/ProductSetTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Archetypes.ProductClasses;
+
+namespace Open.Tests.Archetypes.ProductClasses
+{
+    [TestClass]
+    public class ProductSetTests
+    {
+        private ProductSet set;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            set = new ProductSet();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            set = null;
+        }
+
+        [TestMethod]
+        public void addProductReferenceTest()
+        {
+            set.addProductReference(new ProductIdentifier());
+            Assert.AreEqual(1, set.productIdentifiers.Count);
+        }
+
+        [TestMethod]
+        public void countTest()
+        {
+            Assert.AreEqual(0, set.count());
+        }
+    }
+}
