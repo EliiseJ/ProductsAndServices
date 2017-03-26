e5df9a15e03637fd2a4616180ad1dea6bf023b7b
EliiseJ <juhansoo.eliise@gmail.com>
2017-03-26 17:06:24 +0300

    PackageType klass ja testid

--- Archetypes/ProductClasses/PackageType.cs
+++ Archetypes/ProductClasses/PackageType.cs
@@ -1,14 +1,41 @@
-﻿using Archetypes.ProductClasses;
+﻿using System;
+using System.Linq;
+using Archetypes.ProductClasses;
 
 namespace Open.Archetypes.ProductClasses
 {
-    public class PackageType //: ProductType
+    public class PackageType
     {
-        public PricingStrategy PricingStrategy { get; set; }
 
-        public PackageType() { }
+        public void addProductType(ProductType product)
+        {
+            ProductTypes.Instance.Add(product);
+        }
 
-        
+        public ProductType[] getComponents()
+        {
+            return ProductTypes.Instance.ToArray();
+        }
+
+        public Price[] getPrices()
+        {
+            return Prices.Instance.ToArray();
+        }
+
+        /*public void addProductSet(ProductSet set)
+        {
+            ProductSets.Instance.Add(set);
+        }*/
+
+        /*public void addPropositionOfinclusion(PropositionOfInclusion proposition)
+        {
+            
+        }*/
+
+        public Boolean validatePackage()
+        {
+            return false;
+        }
     }
 }
 
--- Tests/Archetypes/ProductClasses/PackageTypeTests.cs
+++ Tests/Archetypes/ProductClasses/PackageTypeTests.cs
@@ -1,4 +1,5 @@
-﻿using Archetypes.ProductClasses;
+﻿using System.Linq;
+using Archetypes.ProductClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Open.Archetypes.ProductClasses;
 
@@ -14,7 +15,7 @@ namespace Tests.Archetypes.ProductClasses
         [TestInitialize]
         public void InitializeTest()
         {
-            packageType = new PackageType(PricingStrategy.Aggregated);
+            packageType = new PackageType();
         }
 
         [TestCleanup]
@@ -24,11 +25,39 @@ namespace Tests.Archetypes.ProductClasses
         }
 
         [TestMethod]
+        public void addProductTypeTest()
+        {
+            packageType.addProductType(new ProductType());
+            Assert.AreEqual(1, ProductTypes.Instance.Count);
+        }
+
+        [TestMethod]
+        public void getComponentsTest()
+        {
+            packageType.addProductType(new ProductType());
+            Assert.AreEqual(ProductTypes.Instance.Get(0), packageType.getComponents()[0]);
+        }
+
+        [TestMethod]
+        public void getPricesTest()
+        {
+            Prices.Instance.Add(new Price());
+            Assert.AreEqual(Prices.Instance.Get(0), packageType.getPrices()[0]);
+        }
+
+        [TestMethod]
+        public void validatePackageTest()
+        {
+            Assert.AreEqual(false, packageType.validatePackage());
+        }
+
+
+        /*[TestMethod]
         public void PackageTypeConstructorTest()
         {
             expected = "Aggregated";
             actual = packageType.PricingStrategy.ToString();
             Assert.AreEqual(expected, actual);
-        }
+        }*/
     }
 }
