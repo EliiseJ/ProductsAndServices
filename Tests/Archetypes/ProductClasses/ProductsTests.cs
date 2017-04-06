73cda5ab183c5abbe2f351ec88d596fe43b0d42e
sander541 <sandersven1@Gmail.com>
2017-04-06 12:40:53 +0300

    ProductsTests

---
+++ Tests/Archetypes/ProductClasses/ProductsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Aids;
+using Open.Archetypes.ProductClasses;
+namespace Open.Tests.Archetypes.ProductClasses
+{
+    [TestClass]
+    public class ProductsTests : ClassTests<IProduct>
+    {
+        private Products products;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            products = new Products();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            products = null;
+        }
+
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(products);
+        }
+
+        [TestMethod]
+        public void InstanceTest()
+        {
+            TestSingleton(() => Products.Instance);
+        }
+    }
+}
