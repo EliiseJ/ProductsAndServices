1ad1717ee5cd0db71180ba8a2c4e19fb0fe0a560
sander541 <sandersven1@Gmail.com>
2017-04-13 10:52:41 +0300

    ProductSetTests

--- Tests/Archetypes/ProductClasses/ProductSetTests.cs
+++ Tests/Archetypes/ProductClasses/ProductSetTests.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Open.Archetypes.ProductClasses;
 namespace Open.Tests.Archetypes.ProductClasses
 {
     [TestClass]
-    public class ProductSetTests : ClassTests<ProductSet>
+    public class ProductSetTests : CommonTests<ProductSet>
     {
+        protected override ProductSet GetRandomObj()
+        {
+            return ProductSet.Random();
+        }
         [TestMethod]
         public void ConstructorTest()
         {
@@ -12,17 +17,29 @@ namespace Open.Tests.Archetypes.ProductClasses
             Assert.AreEqual(a, typeof(ProductSet));
         }
 
-        //[TestMethod]
-        //public void AddProductReferenceTest()
-        //{
-        //    set.AddProductReference(new ProductIdentifier());
-        //    Assert.AreEqual(1, set.ProductIdentifiers.Count);
-        //}
+        [TestMethod]
+        public void AddProductReferenceTest()
+        {
+            Obj.AddProductReference(new ProductIdentifier());
+            Assert.AreEqual(1, Obj.ProductIdentifiers.Count);
+        }
 
-        //[TestMethod]
-        //public void CountTest()
-        //{
-        //    Assert.AreEqual(0, set.Count());
-        //}
+        [TestMethod]
+        public void CountTest()
+        {
+            Assert.AreEqual(0, Obj.Count());
+        }
+
+        [TestMethod]
+        public void NameTest()
+        {
+            TestProperty(() => Obj.Name, x => Obj.Name = x);        
+        }
+
+        [TestMethod]
+        public void ProductIdentifiersTest()
+        {
+            TestSingleton(() => Obj.ProductIdentifiers);
+        }
     }
 }
