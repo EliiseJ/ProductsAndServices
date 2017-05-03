c51e0fdca439d1a5a3af4e49204ecb3e6368bab2
EliiseJ <juhansoo.eliise@gmail.com>
2017-05-03 22:05:12 +0300

    ProductDetailsModelTests

---
+++ Tests/Logic/ProductClasses/ProductDetailsModelTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Archetypes.ProductClasses;
+using Open.Logic.ProductClasses;
+
+namespace Open.Tests.Logic.ProductClasses
+{
+    [TestClass]
+    public class ProductDetailsModelTests : ClassTests<ProductDetailsModel>
+    {
+        private ProductInstance product;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            product = ProductInstance.Random();
+        }
+
+        [TestMethod]
+        public void NameTest()
+        {
+            var obj = new ProductDetailsModel(product);
+            TestProperty(() => obj.Name, x => obj.Name = x, product.Name);
+        }
+
+        [TestMethod]
+        public void GenreTest()
+        {
+            var obj = new ProductDetailsModel(product);
+            TestProperty(() => obj.Genre, x => obj.Genre = x, product.TypeId);
+        }
+    }
+}
+
